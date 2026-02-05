0d960fdfd0b7c304a9c7f64d6fdb600a6619dde2
Nitya Gurung <nityagurung@Mac-2.broadband>
2026-02-05 11:18:57 +0000

    Fix ALLOWED_HOSTS for Railway

--- fypApp/settings.py
+++ fypApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-h3y$2hhs5+)ez!zis5$f!vx6gh5lk%90q2ols6hgoz0g648(+j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [], os.environ.get("ALLOWED_HOSTS", "127.0.0.1.localhost").split(",")
 
 
 # Application definition
